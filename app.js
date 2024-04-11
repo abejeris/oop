401d7e773854b5f2d5dbf98b215da7f5afb854c8
Audrius Bejeris <audriusbejeris@gmail.com>
2024-04-11 20:34:49 +0300

    added super and extends examples

--- app.js
+++ app.js
@@ -14,16 +14,39 @@
 // =============================================================
 // using Class constructor functions
 
-class Color {
-	constructor(r, g, b) {
-		this.r = r;
-		this.g = g;
-		this.b = b;
-	}
-	rgb() {
-		const { r, g, b } = this;
-		return `rgb(${r}, ${g}, ${b})`;
+// class Color {
+// 	constructor(r, g, b) {
+// 		this.r = r;
+// 		this.g = g;
+// 		this.b = b;
+// 	}
+// 	rgb() {
+// 		const { r, g, b } = this;
+// 		return `rgb(${r}, ${g}, ${b})`;
+// 	}
+// }
+
+// new Color(255, 100, 30);
+// ===================================================================
+
+// extends and super
+
+class Pet {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
 	}
 }
 
-new Color(255, 100, 30);
+class Cat extends Pet {
+	constructor(name, age, livesLeft = 9) {
+		super(name, age);
+		this.livesLeft = livesLeft;
+	}
+	eat() {
+		return `${this.name} eats his food`;
+	}
+	info() {
+		return `${this.name} is ${this.age} years old and it has ${this.livesLeft} lives left`;
+	}
+}
